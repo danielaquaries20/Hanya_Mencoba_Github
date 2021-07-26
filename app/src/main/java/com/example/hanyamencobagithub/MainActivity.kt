4a8aeb9e7aa94161b8c7d83fdc5b284122eb9914
Daniel Pratama <danielaquaries@gmail.com>
2021-07-26 13:49:36 +0700

    Coba Debug dan Release

--- app/src/main/java/com/example/hanyamencobagithub/MainActivity.kt
+++ app/src/main/java/com/example/hanyamencobagithub/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.hanyamencobagithub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonNext : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        buttonNext = findViewById(R.id.btnNext)
+
+        buttonNext.setOnClickListener {
+            val maju = Intent(this, MainActivity2::class.java)
+            startActivity(maju)
+        }
+
+
+
     }
 }
